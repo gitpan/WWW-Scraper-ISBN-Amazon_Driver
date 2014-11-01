3a8dd2851172fa5b6346243e72f60a130e7b2623
Barbie <barbie@missbarbell.co.uk>
2014-11-01 02:06:17 +0000

    Import of BARBIE/WWW-Scraper-ISBN-Amazon_Driver-0.41 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN-Amazon_Driver
    gitpan-cpan-version:      0.41
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-Amazon_Driver-0.41.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/WWW/Scraper/ISBN/AmazonUK_Driver.pm
+++ lib/WWW/Scraper/ISBN/AmazonUK_Driver.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = '0.40';
+$VERSION = '0.41';
 
 #--------------------------------------------------------------------------
 
@@ -185,8 +185,8 @@ sub _parse {
         $data->{content} =~ s/Amazon\.co\.uk.*?://i;
         $data->{content} =~ s/: Books.*//i;
         ($data->{title},$data->{author}) = split(/\s+by\s+/,$data->{content});
-        $data->{title}  =~ s/^Buy\s+//;
-        $data->{author} =~ s/\s*\(.*//;
+        $data->{title}  =~ s/^Buy\s+//  if($data->{title});
+        $data->{author} =~ s/\s*\(.*//  if($data->{author});
     }
 
     ($data->{publisher},$data->{pubdate}) = ($data->{published} =~ /\s*(.*?)(?:;.*?)?\s+\((.*?)\)/) if($data->{published});
